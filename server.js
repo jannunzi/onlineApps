b8472ba5e868df774c1208b75b90ecad96645d36
jannunzi <jannunzi@gmail.com>
2014-10-06 15:17:45 -0400

    Get environment variables

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 // load webservice and database libraries
 var express = require('express');
+//var mongojs = require('mongojs');
 
 // instantiate both libraries and connecto to the cs5610353 database
 var app = express();
+//var db = mongojs("cs5610353", ["serviceClients"]);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.get("/hello", function (req, res) {
-    res.send("Hello World");
+app.get("/env", function (req, res) {
+    res.json(process.env);
 });
 
 // listen to port 3000 in localhost
