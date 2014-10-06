0541151a3391f98e5b6183d07b7f0c4c3beeafcb
jannunzi <jannunzi@gmail.com>
2014-10-06 16:05:52 -0400

    Created Application Module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // load webservice and database libraries
 var express = require('express');
 var mongojs = require('mongojs');
+var applications = require('./public/features/services/server.js');
 
 // instantiate both libraries and connecto to the cs5610353 database
 var app = express();
@@ -11,12 +12,14 @@ app.use(express.static(__dirname + '/public'));
 // configure express to parse JSON in the body of an HTTP request
 app.use(express.bodyParser());
 
-//var mongodbConnectionString = "mongodb://admin:UpwCdvF5cYQa@127.12.162.130:27017/cs5610353";
-var mongodbConnectionString = "mongodb://admin:UpwCdvF5cYQa@127.12.162.130:27017/applications";
-//if (typeof process.env.OPENSHIFT_MONGODB_DB_URL == "undefined") {
-//    mongodbConnectionString = "cs5610353";
-//}
+var mongodbConnectionString = "mongodb://admin:UpwCdvF5cYQa@127.12.162.130:27017/cs5610353";
+if (typeof process.env.OPENSHIFT_MONGODB_DB_URL == "undefined") {
+    mongodbConnectionString = "cs5610353";
+}
 var db = mongojs(mongodbConnectionString, ["serviceClients"]);
+
+
+applications(app, db, mongojs);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
@@ -24,66 +27,6 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get("/env", function (req, res) {
     res.json(process.env);
 });
-
-// map incoming HTTP URL patterns to execute various functions
-// handle HTTP GET request to read all serviceClients from the database
-app.get("/serviceClients", function (req, res) {
-    db.serviceClients.find(function (err, docs) {
-        res.json(docs);
-    });
-});
-
-// handle HTTP POST request to insert new serviceClients into the database
-app.post("/serviceClients", function (req, res) {
-    // the serviceClient is in the body of the HTTP request
-    var svc = req.body;
-
-    // insert new serviceClient object into the database collection serviceClients
-    db.serviceClients.insert(req.body, function (err, doc) {
-        // respond with the new object that has been inserted
-        res.json(doc);
-    });
-});
-
-// handle HTTP GET request for a single serviceClient with :id parameter
-app.get("/serviceClients/:id", function (req, res) {
-    // parse id from the path parameter
-    var id = req.params.id;
-    // select the single document from the database
-    db.serviceClients.findOne({ _id: mongojs.ObjectId(id) }, function (err, doc) {
-        // respond with the document retrieved from the database
-        res.json(doc);
-    });
-});
-
-// handle HTTP PUT request to update serviceClient instance with :id parameter
-app.put("/serviceClients/:id", function (req, res) {
-    db.serviceClients.findAndModify({
-        // find the object by id
-        query: { _id: mongojs.ObjectId(req.params.id) },
-        // new values are in req.body, update it's name
-        update: { $set: { name: req.body.name } },
-        // single one
-        new: true
-    }, function (err, doc, lastErrorObject) {
-        // respond with the new document
-        res.json(doc);
-    });
-});
-
-// handle HTTP DELETE request to remove a serviceClient with :id parameter
-app.delete("/serviceClients/:id", function (req, res) {
-    // parse id from the path parameter
-    var id = req.params.id;
-    // find the document by id and remove it
-    db.serviceClients.remove({ _id: mongojs.ObjectId(id) },
-		function (err, doc) {
-		    // respond with number of documents affected
-		    res.json(doc);
-		});
-});
-
-
 
 // listen to port 3000 in localhost
 app.listen(port, ipaddress);
