87355e4e6ae2a1d0e63c96464c9b1c4c2a1f7ceb
jannunzi <jannunzi@gmail.com>
2014-10-06 15:23:09 -0400

    Added MongoJS

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // load webservice and database libraries
 var express = require('express');
-//var mongojs = require('mongojs');
+var mongojs = require('mongojs');
 
 // instantiate both libraries and connecto to the cs5610353 database
 var app = express();
-//var db = mongojs("cs5610353", ["serviceClients"]);
+
+var mongodbConnectionString = process.env.OPENSHIFT_MONGODB_DB_URL + "cs5610353";
+if (typeof process.env.OPENSHIFT_MONGODB_DB_URL == "undefined") {
+    mongodbConnectionString = "cs5610353";
+}
+var db = mongojs(mongodbConnectionString, ["serviceClients"]);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
